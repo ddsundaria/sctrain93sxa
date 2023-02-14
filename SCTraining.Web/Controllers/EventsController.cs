cd7d0fb9621aa81f1ec291319db4462708a3bc17
ddsundaria <ddsundaria@gmail.com>
2023-02-14 09:38:17 +0100

    code for day 2

---
+++ SCTraining.Web/Controllers/EventsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SCTraining.Web.Models;
+using Sitecore.SecurityModel;
+
+namespace SCTraining.Web.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            EventsRenderingViewModel model = new EventsRenderingViewModel();
+            model.Item = Sitecore.Context.Item;
+
+        //    using (new SecurityDisabler()
+        //    {
+        //         model.Item.Editing.BeginEdit();
+
+        //    model.Item.Fields["EventName"].Value = "Sitecore Training 10.2";
+
+        //    model.Item.Editing.EndEdit();
+        //})
+           
+
+            return View(model);
+    }
+}
+}
---
+++ SCTraining.Web/Models/EventsRenderingViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Web.UI.WebControls;
+
+namespace SCTraining.Web.Models
+{
+    public class EventsRenderingViewModel
+    {
+        public Item Item { get; set; }
+        public HtmlString EventName
+        {
+            get
+            {
+                return new HtmlString(FieldRenderer.Render(this.Item, "EventName"));
+            }
+        }
+        public HtmlString EventDate
+        {
+            get
+            {
+                return new HtmlString(FieldRenderer.Render(this.Item, "EventDate"));
+            }
+        }
+        public HtmlString EventDescription
+        {
+            get
+            {
+                return new HtmlString(FieldRenderer.Render(this.Item, "EventDescription"));
+            }
+        }
+    }
+}
